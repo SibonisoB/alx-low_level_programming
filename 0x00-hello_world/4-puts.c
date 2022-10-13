53e70f0563a204d55e0d9a81e1c623fff5b65edb
SibonisoB <brandenmbhele>
2022-10-13 16:28:52 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,8 @@
 
 /**
  * main - Entry point
- *
- * return: Always return 0 (Success)
+ * Return: Always return 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
