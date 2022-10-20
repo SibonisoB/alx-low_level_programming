710640e65dfb5849c9d3d2a185d49773b490c97c
SibonisoB <brandenmbhele@gmail.com>
2022-10-20 04:49:33 -0700

    Test positive_or_negative function

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifdef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * positive_or_negative - test that correct output is received when given a case of zero
+ */
+void positive_or_negative(int i);
+
+#endif /*MAIN_H*/
